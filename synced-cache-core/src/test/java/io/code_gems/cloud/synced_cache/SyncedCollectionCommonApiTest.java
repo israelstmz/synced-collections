f00ad1622b9e73cd16a2c250f7046f0537b0794b
israels <israels@checkpoint.com>
2022-10-28 15:26:53 +0300

    common API tests refactoring

---
+++ synced-cache-core/src/test/java/io/code_gems/cloud/synced_cache/SyncedCollectionCommonApiTest.java
@@ -0,0 +1,125 @@
+package io.code_gems.cloud.synced_cache;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@SuppressWarnings({"ConstantConditions", "RedundantCollectionOperation"})
+@DisplayName("synced collection common API behaviour should be identical to 'Collection.unmodifiableCollection()':")
+class SyncedCollectionCommonApiTest {
+
+    public static final String ITEM_1 = "item1";
+    public static final String ITEM_2 = "item2";
+
+    @Test
+    @DisplayName("'contains' behavior")
+    void contains() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertThat(testedCollection.contains(ITEM_1)).isEqualTo(unmodifiableCollection.contains(ITEM_1));
+        assertThat(testedCollection.contains(ITEM_2)).isEqualTo(unmodifiableCollection.contains(ITEM_2));
+        assertThat(testedCollection.containsAll(Collections.singleton(ITEM_1)))
+                .isEqualTo(unmodifiableCollection.containsAll(Collections.singleton(ITEM_1)));
+        assertThat(testedCollection.containsAll(Collections.singleton(ITEM_2)))
+                .isEqualTo(unmodifiableCollection.containsAll(Collections.singleton(ITEM_2)));
+    }
+
+    @Test
+    @DisplayName("'size' behavior")
+    void size() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertThat(testedCollection).hasSameSizeAs(unmodifiableCollection);
+    }
+
+    @Test
+    @DisplayName("'isEmpty' behavior")
+    void isEmpty() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.emptyList());
+        var testedCollection = createTestedCollectionOf(Collections.emptyList());
+
+        assertThat(testedCollection.isEmpty()).isEqualTo(unmodifiableCollection.isEmpty());
+
+        unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertThat(testedCollection.isEmpty()).isEqualTo(unmodifiableCollection.isEmpty());
+    }
+
+    @Test
+    @DisplayName("'stream' behavior")
+    void stream() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertThat(testedCollection.stream().anyMatch(a -> a.equals(ITEM_1)))
+                .isEqualTo(unmodifiableCollection.stream().anyMatch(a -> a.equals(ITEM_1)));
+        assertThat(testedCollection.stream().anyMatch(a -> a.equals(ITEM_2)))
+                .isEqualTo(unmodifiableCollection.stream().anyMatch(a -> a.equals(ITEM_2)));
+    }
+
+    @Test
+    @DisplayName("'add' behavior")
+    void add() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertEqualFailure(() -> testedCollection.add(ITEM_2), () -> unmodifiableCollection.add(ITEM_2), "add");
+        assertEqualFailure(() -> testedCollection.addAll(Collections.singleton(ITEM_2)),
+                () -> unmodifiableCollection.addAll(Collections.singleton(ITEM_2)), "addAll");
+    }
+
+    @Test
+    @DisplayName("'remove' behavior")
+    void remove() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertEqualFailure(() -> testedCollection.remove(ITEM_1), () -> unmodifiableCollection.remove(ITEM_1), "remove");
+        assertEqualFailure(() -> testedCollection.removeAll(Collections.singleton(ITEM_1)),
+                () -> unmodifiableCollection.removeAll(Collections.singleton(ITEM_1)), "removeAll");
+    }
+
+    @Test
+    @DisplayName("'clear' behavior")
+    void clear() {
+        var unmodifiableCollection = createUnmodifiableCollectionOf(Collections.singleton(ITEM_1));
+        var testedCollection = createTestedCollectionOf(Collections.singleton(ITEM_1));
+
+        assertEqualFailure(testedCollection::clear, unmodifiableCollection::clear, "clear");
+
+    }
+
+    private static Collection<String> createUnmodifiableCollectionOf(Collection<String> c) {
+        return Collections.unmodifiableCollection(c);
+    }
+
+    private SyncedCollection<String> createTestedCollectionOf(Collection<String> c) {
+        var syncSupplier = new MockSyncCollectionSupplier<String>();
+        var testedCollection = SyncedCollection.create(syncSupplier);
+        syncSupplier.mockSupplyWith(c);
+        testedCollection.startSync();
+        return testedCollection;
+    }
+
+    private void assertEqualFailure(Runnable testedAction, Runnable expectedAction, String actionName) {
+        try {
+            testedAction.run();
+            fail("tested action did not fail: " + actionName);
+        } catch (Exception actual) {
+            try {
+                expectedAction.run();
+                fail("expected action did not fail: " + actionName);
+            } catch (Exception expected) {
+                assertThat(actual).isInstanceOf(expected.getClass());
+            }
+        }
+    }
+}
