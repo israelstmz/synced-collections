ba588201eee13e6b68d283db0a0430d48cd55128
israels <israels@checkpoint.com>
2022-10-28 15:05:43 +0300

    collection all basic behavior

--- synced-cache-core/src/main/java/io/code_gems/cloud/synced_cache/InMemSyncedCollection.java
+++ synced-cache-core/src/main/java/io/code_gems/cloud/synced_cache/InMemSyncedCollection.java
@@ -3,6 +3,11 @@ package io.code_gems.cloud.synced_cache;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 class InMemSyncedCollection<E> implements SyncedCollection<E> {
 
@@ -18,11 +23,11 @@ class InMemSyncedCollection<E> implements SyncedCollection<E> {
     }
 
     public int size() {
-        return 0;
+        return collection.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return collection.isEmpty();
     }
 
     public boolean contains(Object o) {
@@ -30,15 +35,15 @@ class InMemSyncedCollection<E> implements SyncedCollection<E> {
     }
 
     public Iterator<E> iterator() {
-        return null;
+        return collection.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return collection.toArray();
     }
 
     public <T> T[] toArray(T[] a) {
-        return null;
+        return collection.toArray(a);
     }
 
     public boolean add(E e) {
@@ -46,26 +51,72 @@ class InMemSyncedCollection<E> implements SyncedCollection<E> {
     }
 
     public boolean remove(Object o) {
-        return false;
+        return collection.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return collection.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        return collection.addAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return collection.removeAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return collection.retainAll(c);
     }
 
     public void clear() {
-
+        collection.clear();
     }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return collection.toArray(generator);
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        return collection.removeIf(filter);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return collection.spliterator();
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return collection.stream();
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return collection.parallelStream();
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        collection.forEach(action);
+    }
+
+    @Override
+    public int hashCode() {
+        return collection.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return collection.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return collection.toString();
+    }
+
 }
