122292d5997dfc68e9d9847f4abeb963d203a8a0
israels <israels@checkpoint.com>
2022-11-01 18:13:39 +0200

    handle outOfSync in all other accessor actions

--- synced-cache-core/src/main/java/io/code_gems/cloud/synced_cache/StrictSyncedCollection.java
+++ synced-cache-core/src/main/java/io/code_gems/cloud/synced_cache/StrictSyncedCollection.java
@@ -5,7 +5,11 @@ import lombok.Builder;
 
 import java.time.Duration;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class StrictSyncedCollection<E> extends InMemSyncedCollection<E> {
 
@@ -25,6 +29,42 @@ public class StrictSyncedCollection<E> extends InMemSyncedCollection<E> {
     public boolean containsAll(Collection<?> c) {
         verifyCollectionIsSynced();
         return super.containsAll(c);
+    }
+
+    @Override
+    public int size() {
+        verifyCollectionIsSynced();
+        return super.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        verifyCollectionIsSynced();
+        return super.isEmpty();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        verifyCollectionIsSynced();
+        return super.iterator();
+    }
+
+    @Override
+    public Stream<E> stream() {
+        verifyCollectionIsSynced();
+        return super.stream();
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        verifyCollectionIsSynced();
+        return super.spliterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        verifyCollectionIsSynced();
+        super.forEach(action);
     }
 
     private void verifyCollectionIsSynced() {
--- synced-cache-core/src/test/java/io/code_gems/cloud/synced_cache/SyncCollectionSyncBehaviorTest.java
+++ synced-cache-core/src/test/java/io/code_gems/cloud/synced_cache/SyncCollectionSyncBehaviorTest.java
@@ -1,17 +1,23 @@
 package io.code_gems.cloud.synced_cache;
 
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@SuppressWarnings({"ResultOfMethodCallIgnored", "RedundantCollectionOperation"})
 @DisplayName("Sync behavior:")
 class SyncCollectionSyncBehaviorTest {
 
@@ -59,38 +65,29 @@ class SyncCollectionSyncBehaviorTest {
         await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> assertThat(testedCollection.contains(ITEM_1)).isTrue());
     }
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    @Nested
-    @DisplayName("in case no sync ever succeeded:")
-    class NoSyncEverSucceeded {
+    @SuppressWarnings("unused")
+    @ParameterizedTest(name = "''{1}''")
+    @DisplayName("in case no sync ever succeeded, following accessor actions should fail:")
+    @MethodSource
+    void accessorActionsShouldFail(ThrowableAssert.ThrowingCallable action, String actionName) {
+        await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
+                assertThatThrownBy(action).isInstanceOf(OutOfSyncException.class));
+    }
 
-        @BeforeEach
-        void setUp() {
-            mockSupplier = new MockSyncCollectionSupplier<>();
-            mockSupplier.mockSupplyFailure();
-            testedCollection = SyncedCollection.build(mockSupplier)
-                                               .interval(Duration.ofMillis(1))
-                                               .build();
-            testedCollection.startSync();
-        }
-
-        @Test
-        @DisplayName("'contains' action should fail with OutOfSyncException")
-        void contains() {
-            await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
-                    assertThatThrownBy(() -> testedCollection.contains(ITEM_1))
-                            .isInstanceOf(OutOfSyncException.class));
-        }
-
-        @Test
-        @DisplayName("'containsAll' action should fail with OutOfSyncException")
-        void containsAll() {
-            var otherCollection = Collections.singleton(ITEM_1);
-            await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
-                    assertThatThrownBy(() -> testedCollection.containsAll(otherCollection))
-                            .isInstanceOf(OutOfSyncException.class));
-        }
-
+    static Stream<Arguments> accessorActionsShouldFail() {
+        var mockSupplier = new MockSyncCollectionSupplier<>();
+        mockSupplier.mockSupplyFailure();
+        var testedCollection = SyncedCollection.build(mockSupplier).interval(Duration.ofMillis(1)).build();
+        testedCollection.startSync();
+        return Stream.of(
+            arguments((ThrowableAssert.ThrowingCallable) () -> testedCollection.contains(ITEM_1), "contains"),
+            arguments((ThrowableAssert.ThrowingCallable) () -> testedCollection.containsAll(Collections.singleton(ITEM_1)), "containsAll"),
+            arguments((ThrowableAssert.ThrowingCallable) () -> testedCollection.forEach(item -> {}), "forEach"),
+            arguments((ThrowableAssert.ThrowingCallable) testedCollection::size, "size"),
+            arguments((ThrowableAssert.ThrowingCallable) testedCollection::iterator, "iterator"),
+            arguments((ThrowableAssert.ThrowingCallable) testedCollection::stream, "stream"),
+            arguments((ThrowableAssert.ThrowingCallable) testedCollection::spliterator, "spliterator")
+        );
     }
 
 }
